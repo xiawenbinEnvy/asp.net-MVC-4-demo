fb638771669b008e16c789aadf1be09835c634f6
wbxia <wbxia@Ctrip.com>
2015-03-27 13:05:28 +0800

    自定义ViewEngine，实现PartialView路径的自动选择

---
+++ asp.net MVC 4扩展点利用demo/MultiPartialViewSelect/MultiPartialViewSelect/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MultiPartialViewSelect
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{channel}",
+                defaults: new { controller = "Home", action = "Index", channel = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ asp.net MVC 4扩展点利用demo/MultiPartialViewSelect/MultiPartialViewSelect/Extensions/MaybeH5PartialViewViewEngine.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace MultiPartialViewSelect.Extensions
+{
+    public class MaybeH5PartialViewViewEngine : RazorViewEngine
+    {
+        public override ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            string channel = controllerContext.RouteData.Values.ContainsKey("channel") ? 
+                controllerContext.RouteData.GetRequiredString("channel") : "";
+            if (!string.IsNullOrEmpty(channel) && channel == "h5")
+            {
+                string controllerName = controllerContext.RouteData.GetRequiredString("controller");
+                string h5ViewPath = string.Format("~/views/{0}/h5/{1}.cshtml", controllerName, partialViewName);
+                string fileName = controllerContext.HttpContext.Request.MapPath(h5ViewPath);
+                if (File.Exists(fileName))
+                {
+                    return new ViewEngineResult(
+                        new RazorView(controllerContext, h5ViewPath, null, false, null), this);
+                }
+            }
+            return base.FindPartialView(controllerContext, partialViewName, useCache);
+        }
+
+        public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            return base.FindView(controllerContext, viewName, masterName, useCache);
+        }
+
+        public override void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            base.ReleaseView(controllerContext, view);
+        }
+    }
+}
